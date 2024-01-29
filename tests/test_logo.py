0bbd4b7ec878c8cfd988a61126df1b4944cbeaa9
VarbanAnastasia <145008875+VarbanAnastasia@users.noreply.github.com>
2024-01-29 12:02:25 +0300

    fixed

--- tests/test_logo.py
+++ tests/test_logo.py
@@ -9,11 +9,10 @@ from page_objects.logo import Logo
 class TestLogo:
     @allure.title("Проверка возврата на главную страницу при нажатии лого Самокат")
     def test_redirection_to_main_page(self, driver):
-        base_page = BasePage(driver)
-        base_page.go_to_site('https://qa-scooter.praktikum-services.ru/')
         logo = Logo(driver)
+        logo.go_to_site('https://qa-scooter.praktikum-services.ru/')
         logo.click_logo_scooter()
-        assert base_page.current_url() == 'https://qa-scooter.praktikum-services.ru/'
+        assert logo.current_url() == 'https://qa-scooter.praktikum-services.ru/'
 
     @allure.title("Проверка возврата на главную страницу Дзен при нажатии лого Яндекс")
     def test_redirection_to_yandex_main_page(self, driver):
